a253cae52197991d0aa44a29c29ce7ce690cc2fb
Deep Chakraborty <deep123aeccse@gmail.com>
2025-02-17 11:44:29 +0530

    First commit

--- src/api/auth.js
+++ src/api/auth.js
@@ -4,11 +4,11 @@ import axios from "axios";
 const API_BASE_URL = process.env.REACT_APP_BACKEND_URL;
 
 export const loginWithGoogle = () => {
-  window.location.href = `${API_BASE_URL}/login/oauth2/authorization/google`;
+  window.location.href = `${API_BASE_URL}/oauth2/authorization/google`;
 };
 
 export const loginWithGitHub = () => {
-  window.location.href = `${API_BASE_URL}/login/oauth2/authorization/github`;
+  window.location.href = `${API_BASE_URL}/oauth2/authorization/github`;
 };
 
 export const logout = async () => {
